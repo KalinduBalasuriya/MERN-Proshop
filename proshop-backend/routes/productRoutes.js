389ba2edaf99c3960dc668bb38e786ef3f67d2e4
KalinduBalasuriya <61897592+KalinduBalasuriya@users.noreply.github.com>
2023-08-21 03:32:50 +0530

    custom error handling

--- proshop-backend/routes/productRoutes.js
+++ proshop-backend/routes/productRoutes.js
@@ -7,23 +7,29 @@ import Product from '../models/productModel.js';
 // Fet all products
 // route GET/api/products
 // access Public
-router.get('/', asyncHandler(async(req, res) => {
-    const products = await Product.find({})
-    res.json(products);
-
-    console.error(`Error: ${error.message}`)
-    process.exit(1)
-}))
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.json(products);
+    }
+    catch {
+        console.error(`Error: ${error.message}`)
+        process.exit(1)
+    }
+})
 
 // Fet single products
 // route GET/api/products/:id
 // access Public
-router.get('/:id', asyncHandler(async(req, res) => {
+router.get('/:id', asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
     if (product) {
         res.json(product)
-    } else
+        console.log('Here i am');
+    } 
+    else{
         res.status(404).json({ message: "Product not found" })
+    }
 }))
 
 export default router;
--- proshop-backend/server.js
+++ proshop-backend/server.js
@@ -8,18 +8,28 @@ connectDB()
 
 const app = express();
 
-[app.use((req,res)=>{
-console.log(req.originalUrl);
-})
-]
 app.get('/',(req,res)=>{
     res.send('API is running....')
 })
 
 app.use('/api/products', productRoutes)
 
+// Move the 404 middleware here
+app.use((req,res,next)=>{
+    console.log('Hi');
+    const error = new Error(`Not Found - ${req.originalUrl}`)
+    res.status(404)
+    next(error)
+})
 
-
+app.use((err,req,res,next)=>{
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    })
+})
 
 const port = process.env.PORT || 5000
 app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`));
